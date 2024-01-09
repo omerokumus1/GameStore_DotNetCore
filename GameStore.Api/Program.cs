d1216c461ce77a883129c016bdaaa4f9a1b5ecff
Ömer Faruk Okumuş <omerokumus3@gmail.com>
2024-01-09 14:59:23 +0300

    GameStoreContext

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,4 +1,5 @@
 
+using GameStore.Api.Data;
 using GameStore.Api.Endpoints;
 using GameStore.Api.Entities;
 using GameStore.Api.Repositories;
@@ -8,10 +9,10 @@ builder.Services
         .AddSingleton<IGamesRepository, GamesRepository>();
 
 var connString = builder.Configuration.GetConnectionString("GameStoreContext");
+builder.Services.AddSqlServer<GameStoreContext>(connString);
 
 var app = builder.Build();
 
 app.MapGamesEndpoints();
 
 app.Run();
- 
