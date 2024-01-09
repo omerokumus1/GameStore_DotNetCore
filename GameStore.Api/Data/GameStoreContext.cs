868cdb91faebe7977d6b65148f1294dc9e6474f7
Ömer Faruk Okumuş <omerokumus3@gmail.com>
2024-01-09 14:59:03 +0300

    DbContext

---
+++ GameStore.Api/Data/GameStoreContext.cs
@@ -0,0 +1,19 @@
+using GameStore.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public class GameStoreContext: DbContext
+{
+    // Initial dbset for games. This represents the table in the database.
+    // We can add more dbsets for other tables.
+    // We can also add more properties to this class for more tables.
+    // We can also add more classes like this for more databases.
+    // We can query the database using these dbsets.
+    public DbSet<Game> Games => Set<Game>();
+    public GameStoreContext(DbContextOptions<GameStoreContext> options)
+        : base(options)
+    {
+
+    }
+}
